1df60d0e8d21dda13a20fc316972442b3a940476
typicalcold <83438890+typicalcold@users.noreply.github.com>
2024-07-07 10:21:08 -0700

    added quick install

---
+++ install.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Installing tk...
+pip install tk
+
+echo Installing pywin32...
+pip install pywin32
+
+echo Installing dearpygui...
+pip install dearpygui
+
+echo Installing pyMeow...
+pip install pyMeow.zip
+echo.
+echo For more information about tkinter, visit:
+echo https://docs.python.org/id/3.12/library/tkinter.html
+
+echo For more information about pywin32, visit:
+echo https://github.com/mhammond/pywin32
+
+echo For more information about dearpygui, visit:
+echo https://github.com/hoffstadt/DearPyGui
+
+echo For more information about pyMeow, visit:
+echo https://github.com/qb-0/pyMeow
+pause
